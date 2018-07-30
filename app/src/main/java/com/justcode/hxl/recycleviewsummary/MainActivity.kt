2055e3639da2fe19750d5baa82a94528083b0980
2214576751@qq.com <hxl19920401>
2018-07-30 11:49:38 +0800

    first commit : creat project

---
+++ app/src/main/java/com/justcode/hxl/recycleviewsummary/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.justcode.hxl.recycleviewsummary
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
