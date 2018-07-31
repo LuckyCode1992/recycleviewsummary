f6114166f4a8752b5ca771658f62960b2187746e
2214576751@qq.com <hxl19920401>
2018-07-31 11:37:07 +0800

    6 commit : 侧滑item完成

--- app/src/main/java/com/justcode/hxl/recycleviewsummary/DaicehuaRecycleActivity.kt
+++ app/src/main/java/com/justcode/hxl/recycleviewsummary/DaicehuaRecycleActivity.kt
@@ -2,11 +2,54 @@ package com.justcode.hxl.recycleviewsummary
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.support.v7.widget.LinearLayoutManager
+import com.recycleview.smartrefresh.layout.footer.ClassicsFooter
+import com.recycleview.smartrefresh.layout.header.ClassicsHeader
+import kotlinx.android.synthetic.main.activity_daicehua_recycle.*
 
 class DaicehuaRecycleActivity : AppCompatActivity() {
-
+    var list: MutableList<Int> = ArrayList()
+    var adapter: MyAdapter? = null
+    var handler = Handler()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_daicehua_recycle)
+
+        for (i in 1..20) {
+            list.add(i)
+        }
+        adapter = MyAdapter(list,this)
+        val linearLayoutManager = LinearLayoutManager(this)
+        linearLayoutManager.orientation = LinearLayoutManager.VERTICAL
+        recycleview.layoutManager = linearLayoutManager
+        recycleview.adapter = adapter
+
+        refreshlayout.setRefreshHeader(ClassicsHeader(this))
+        refreshlayout.setRefreshFooter(ClassicsFooter(this))
+
+        refreshlayout.setOnRefreshListener {
+            handler.postDelayed({
+                list.clear()
+                for (i in 1..10){
+                    list.add(i)
+                }
+                adapter?.update(list)
+                it.finishRefresh()
+
+            },2000)
+
+        }
+        refreshlayout.setOnLoadMoreListener {
+            handler.postDelayed({
+
+                for (i in 101..121){
+                    list.add(i)
+                }
+                adapter?.update(list)
+                it.finishLoadMore()
+
+            },2000)
+        }
     }
 }
--- app/src/main/java/com/justcode/hxl/recycleviewsummary/MyAdapter.kt
+++ app/src/main/java/com/justcode/hxl/recycleviewsummary/MyAdapter.kt
@@ -5,6 +5,7 @@ import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import kotlinx.android.synthetic.main.item.view.*
 
 class MyAdapter(var list: List<Int>, var context: Context) : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
@@ -25,6 +26,17 @@ class MyAdapter(var list: List<Int>, var context: Context) : RecyclerView.Adapte
         with(holder.itemView){
             title.text = "title:"+list[position]
             content.text = "content:"+list[position]
+
+            swipe_layout.isSwipeEnable = true
+
+            left_menu.setOnClickListener {
+                Toast.makeText(context,"点击了左边",Toast.LENGTH_LONG).show()
+                swipe_layout.close()
+            }
+            right_menu.setOnClickListener({
+                Toast.makeText(context,"点击了右边",Toast.LENGTH_LONG).show()
+                swipe_layout.close()
+            })
         }
     }
 
